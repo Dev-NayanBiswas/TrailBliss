a2a5877f9a4fd0d73f1a40bbd54dd8cb0499eb3c
Dev-NayanBiswas <dev.nayan.biswas@gmail.com>
2024-11-18 03:10:18 +0600

    Password toggling implimented

--- src/Components/Navbar/DropDown.jsx
+++ src/Components/Navbar/DropDown.jsx
@@ -7,7 +7,7 @@ function DropDown() {
         <section className="h-[30vh] flex flex-col gap-4 my-4 justify-evenly lg:hidden">
             <NavLink to="/" className={({isActive})=>isActive?"dropActive":"dropInactive"}>Home</NavLink>
             <NavLink to="/aboutUs" className={({isActive})=>isActive? "dropActive":"dropInactive"}>About Us</NavLink>
-                <NavLink to="/contactUs" className={({isActive})=>isActive? "dropActive":"dropInactive"}>Contact Us</NavLink>
+                <NavLink to="/contactUs" className={({isActive})=>isActive? "dropActive":"dropInactive"}>Contact Us</NavLink> 
                 <NavLink to="/portfolio" className={({isActive})=>isActive? "dropActive":"dropInactive"}>Portfolio</NavLink>
                 <NavLink to="/registration" className={({isActive})=>isActive? "dropActive":"dropInactive"}>Registration</NavLink>
                 <section className="text-center w-1/2 mx-auto">
