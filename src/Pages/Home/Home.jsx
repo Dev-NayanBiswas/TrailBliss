e628242072fb7e1b009ff2cf3312d02d9f2b606e
Dev-NayanBiswas <dev.nayan.biswas@gmail.com>
2024-11-20 18:44:06 +0600

    Carousel added and appointment card design done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,15 +13,15 @@ function Home(){
             <Banner/>
           </section>
           <section className="text-center mt-10">
-            <h1 className="md:text-3xl lg:text-5xl w-5/12 mx-auto text-xl text-gray-800/65 font-bold">Discover Eco-Adventure <span className="md:text-3xl lg:text-5xl text-xl font-semibold text-[var(--primary-color)]">Bliss</span></h1>
+            <h1 className="md:text-3xl lg:text-5xl lg:w-5/12 md:w-7/12 w-full mx-auto text-[30px] text-gray-800/65 font-bold">Discover Eco-Adventure <span className="md:text-3xl lg:text-5xl text-[30px] font-semibold text-[var(--primary-color)]">Bliss</span></h1>
             <p className="md:w-7/12 w-10/12 mx-auto my-5 text-sm">Embark on sustainable journeys that connect you with nature. Explore breathtaking trails, unique wildlife, and unforgettable adventures—all while protecting our planet for generations to come. 🌿</p>
           </section>
           <div className="md:w-7/12 w-10/12 mx-auto h-0.5 border-b-[1px] border-[var(--primary-color)] mb-8"></div>
 
-          <section className="my-5">
+          <section id="cards" className="my-5">
             <CategoryMarquee/>
           </section>
-          <section className="min-h-screen">
+          <section  className="min-h-screen">
             <Outlet context={[data]}/>
           </section>
         </section>
